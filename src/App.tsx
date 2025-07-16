76b5704e086dded7dcf20316a3d4bb26e178c05d
Hallou Ghiles <hallou.ghiles@gmail.com>
2025-07-16 15:11:02 +0200

    Correction du chemin de la route pour la page de copie d'ingeris dans le fichier App.tsx, en remplaçant le tiret par un underscore.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/copy-ingeris" element={<Home />} />
+          <Route path="/copy_ingeris" element={<Home />} />
           <Route path="/domains" element={<Domains />} />
           <Route path="/services/*" element={<Services />} />
           <Route path="/careers" element={<Careers />} />
